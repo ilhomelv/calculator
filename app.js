e288ebd19297b776d9a5006afc86d3ae19a68255
ilhomelv <ilhomelv@gmail.com>
2018-03-28 00:30:25 +0500

    changes the app.js from 0 to hero

--- app.js
+++ app.js
@@ -1,64 +1,88 @@
+var  clear = document.getElementById('clear');
+var  back = document.getElementById('back');
+var  divide = document.getElementById('divide');
+var  multiple = document.getElementById('multiple');
+var  minus = document.getElementById('minus');
+var  plus = document.getElementById('plus');
+var  dot = document.getElementById('dot');
+var  percent = document.getElementById('percent');
+var  equal = document.getElementById('equal');
+var  one = document.getElementById('one');
+var  two = document.getElementById('two');
+var  three = document.getElementById('three');
+var  four = document.getElementById('four');
+var  five = document.getElementById('five');
+var  six = document.getElementById('six');
+var  seven = document.getElementById('seven');
+var  eight = document.getElementById('eight');
+var  nine = document.getElementById('nine');
+var  zero = document.getElementById('zero');
+var  doubleZero = document.getElementById('doubleZero');
 
+var  buttonsDiv = document.getElementById('buttonsDiv');
+var  display = document.getElementById('display');
+var arr = [];
+var result;
 
-var inputs = [""];
-var str;
-var opsA = ["+", "-", "/", "*"];
-var opsB = ["."];
-var nums = [0,1,2,3,4,5,6,7,8,9];
-
-
-function getValue(input){
-    if(opsB.includes(inputs[inputs.length-1]===true && inputs===".")){
-
+buttonsDiv.addEventListener("click", doCalc, false);
+function doCalc(e){
+   console.log(e.target.innerText);
+   console.log(e.currentTarget);
+    if(e.currentTarget.value === e.target.value){
+      //do nothing
     }
-    else if (inputs.length===1 && opsA.includes(input)===false) {
-        inputs.push(input);
-    }
-    else if (opsA.includes(inputs[inputs.length-1])===false) {
-      inputs.push(input);
-    }
-    else if (nums.includes(Number(input))) {
-        inputs.push(input);
-    }
-    update();
+     else if(e.target.innerText === "=") {
+       display.innerText = eval(display.innerText);
+     }
+     else if (e.target.innerText === "C") {
+       display.innerText = '';
+     }
+     else if (e.target.innerText === "%") {
+       display.innerText = doPercentageCalc(display.innerText);
+     }
+     else {
+       display.innerText += e.target.innerText;
+     }
 
+
+
+   e.stopPropagation();
 }
 
-function update(){
-  str = inputs.join("");
-  document.getElementById("display").innerHTML = str;
-}
 
-function equals(){
-  if(inputs.length>1) { //if inputs array length is more than 1 element
-    str = inputs.join("");
-    document.getElementById("display").innerHTML = eval(str);
+
+// function doLogic(x){
+//   if(x.includes("+")) {
+//     arr = x.split("+",2);
+//     result = parseInt(arr[0]) + (parseInt(arr[2]));
+//   }
+//
+//   return result;
+// }
+
+
+
+
+
+
+function doPercentageCalc(x){
+  if (x.includes("+")) {
+    arr = x.split("+",2);
+    console.log("#49 " + arr[0] + " + " + arr[1]);
+    result = parseInt(arr[0]) + (parseInt(arr[1]) * parseInt(arr[0]) /100);
   }
-}
-
-document.getElementById('buttonsDiv').addEventListener('click', handler, false);
-
-function handler(e){
-  if(e.target !== e.currentTarget){ //if the value is not div element
-      if(e.target.id === "clear"){
-        inputs=[""];
-        update();
-      }
-      else if (e.target.id === "back") {
-        inputs.pop();
-        update();
-      }
-      else if (e.target.id ==="=") {
-          equals();
-      }
-    else {
-      if(inputs[inputs.length-1].indexOf("+", "-", "/", "*", ".")===-1){
-              getValue(e.target.id );
-      }
-      else{
-              getValue(e.target.id );
-      }
-    }
+  else if (x.includes("-")) {
+    arr = x.split("-",2);
+    console.log("#49 " + arr[0] + " - " + arr[1]);
+    result = parseInt(arr[0]) - (parseInt(arr[1]) * parseInt(arr[0]) /100);
   }
-  e.stopPropagation();//stop event processing and going up on DOM
+  else if (x.includes("*")) {
+    arr = x.split("*",2);
+    console.log("#49 " + arr[0] + " * " + arr[1]);
+    result = parseInt(arr[1]) * parseInt(arr[0]) /100;
+  }
+  else {
+    //error
+  }
+  return result;
 }
